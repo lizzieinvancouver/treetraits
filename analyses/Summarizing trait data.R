eb68dda9efd3ea07424cc93575e428cd3bd0bdb1
Dan Flynn <danfbflynn@gmail.com>
2016-08-24 09:44:17 -0400

    prepping trait data

--- analyses/Summarizing trait data.R
+++ analyses/Summarizing trait data.R
@@ -1,4 +1,8 @@
 # Compiled tree traits
+
+library(ggplot2)
+library(xtable)
+
 setwd("~/Documents/git/treetraits/")
 d <- read.csv("analyses/input/Summer 2015 Tree Traits.csv")
 
@@ -6,8 +10,6 @@ length(unique(d$Species))
 
 
 dt <- with(d, table(Species, Site))
-
-
 
 # Species we have at only one site
 
@@ -29,13 +31,22 @@ d$wd <- d$Stem.mass/d$Stem.volume
 d$height = d$Height
 d$height = as.numeric(sub("<1",0.5, as.character(d$height)))
 
+# Clean DBH, to get a sum DBH across all stems
+
+dbh <- vector() # i = 56 and 57 test
+for(i in 1:nrow(d)){
+  dx <- d[i, grep("DBH", names(d))]
+  dsplit <- unlist(apply(dx, 1, function(x) strsplit(x, ",")))
+  dbh <- c(dbh, sum(as.numeric(sub("<1", 0.5, dsplit))))
+  }
+
+d$DBH = dbh
+
 # Plotting
 
 plot(sla ~ height, data = d)
 
 # Four sites
-
-library(ggplot2)
 
 ggplot(d, aes(DBH, height, color = Site)) + geom_point()
 
@@ -66,10 +77,6 @@ xtable(data.frame(sort(rowSums(dt),T)))
 # Total samples per site
 xtable(data.frame(sort(colSums(dt),T)))
 
-<<<<<<< Updated upstream
-# Species per site
-xtable(apply(dt, 2, function(x) length(x[x>0])))
-=======
 ## from buds
 
 (leafcount <- with(d, tapply(Leaf.area, list(Species, Site), function(x) length(x[!is.na(x)]))))
@@ -77,26 +84,3 @@ xtable(apply(dt, 2, function(x) length(x[x>0])))
 rowSums(leafcount, na.rm=T)
 colSums(leafcount, na.rm=T)
 
-# <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <>
-# 3. Individual level
-# look at consistency of performance within individuals, across treatments, as measure of plasticity.
-
-# Is consistency related to earlier leafout?
-
-vars <- aggregate(lday ~ sp + site + ind + wd + sla + X.N + Pore.anatomy, FUN = function(x) sd(x, na.rm=T) 
-                  / mean(x, na.rm=T)
-                  , data = dxt)
-
-# remove extreme values 
-
-vars$day = lday.agg[match(vars$sp, lday.agg$sp),"lday"]
-vars$site = as.factor(vars$site); levels(vars$site) = c("HF","SH")
-summary(lmer(lday ~ day + (1|sp), data = vars))
-
-pdf(file.path(figpath, "indvar.pdf"), width = 5, height = 5)
-
-ggplot(vars, aes(day, lday, group = site)) + geom_point(aes(col=site)) + geom_smooth(method = "lm") + 
-  xlab("Day of leafout in Warm/Long") + ylab("CV of leafout across treatments within individuals") 
-
-dev.off();system(paste("open", file.path(figpath, "indvar.pdf"), "-a /Applications/Preview.app"))
->>>>>>> Stashed changes
